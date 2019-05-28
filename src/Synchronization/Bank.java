4b36bdd99871c49bc710eff2605182338010a210
LedSinh <dinhsinh.le@outlook.com>
2019-05-29 00:17:59 +0700

    Synchronized with Lock

--- src/Synchronization/Bank.java
+++ src/Synchronization/Bank.java
@@ -1,25 +1,42 @@
 package Synchronization;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Bank {
 	private final double[] accounts;
+	private Lock bankLock = new ReentrantLock();
+	private Condition sufficientFunds;
 	public Bank(int n, double initialBalance) {
 		accounts = new double[n];
 		for (int i = 0; i < accounts.length; i++)
 			accounts[i] = initialBalance;
+		bankLock = new ReentrantLock();
+		sufficientFunds = bankLock.newCondition();
 	}
 	/**
 	* Transfers money from one account to another.
 	* @param from the account to transfer from
 	* @param to the account to transfer to
 	* @param amount the amount to transfer
+	 * @throws InterruptedException 
 	*/
-	public void transfer(int from, int to, double amount) {
-		if (accounts[from] < amount) return;
-		System.out.print(Thread.currentThread());
-		accounts[from] -= amount;
-		System.out.printf(" %10.2f from %d to %d", amount, from, to);
-		accounts[to] += amount;
-		System.out.printf(" Total Balance: %10.2f%n", getTotalBalance());
+	public void transfer(int from, int to, double amount) throws InterruptedException {
+		bankLock.lock();
+		try {
+			while (accounts[from] < amount)
+				sufficientFunds.await();
+			System.out.print(Thread.currentThread());
+			accounts[from] -= amount;
+			System.out.printf(" %10.2f from %d to %d", amount, from, to);
+			accounts[to] += amount;
+			System.out.printf(" Total Balance: %10.2f%n", getTotalBalance());
+			sufficientFunds.signalAll();
+		}
+		finally {
+			bankLock.unlock();
+		}
 	}
 	/**
 	* Gets the sum of all account balances.
@@ -27,10 +44,17 @@ public class Bank {
 	*/
 	public double getTotalBalance()
 	{
-		double sum = 0;
-		for (double a : accounts)
-		sum += a;
-		return sum;
+		bankLock.lock();
+		try {
+			double sum = 0;
+			for (double a : accounts)
+			sum += a;
+			return sum;
+		}
+		finally {
+			bankLock.unlock();
+		}
+		
 	}
 	/**
 	* Gets the number of accounts in the bank.
