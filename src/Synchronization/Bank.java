46c933e4cb0990c8689ff663af91cc4b5e925cb7
LedSinh <dinhsinh.le@outlook.com>
2019-05-29 09:20:19 +0700

    synchronized method

--- src/Synchronization/Bank.java
+++ src/Synchronization/Bank.java
@@ -22,27 +22,21 @@ public class Bank {
 	* @param amount the amount to transfer
 	 * @throws InterruptedException 
 	*/
-	public void transfer(int from, int to, double amount) throws InterruptedException {
-		bankLock.lock();
-		try {
+	public synchronized void transfer(int from, int to, double amount) throws InterruptedException {
 			while (accounts[from] < amount)
-				sufficientFunds.await();
+			wait();
 			System.out.print(Thread.currentThread());
 			accounts[from] -= amount;
 			System.out.printf(" %10.2f from %d to %d", amount, from, to);
 			accounts[to] += amount;
 			System.out.printf(" Total Balance: %10.2f%n", getTotalBalance());
-			sufficientFunds.signalAll();
-		}
-		finally {
-			bankLock.unlock();
-		}
+			notifyAll();
 	}
 	/**
 	* Gets the sum of all account balances.
 	* @return the total balance
 	*/
-	public double getTotalBalance()
+	public synchronized double getTotalBalance()
 	{
 		bankLock.lock();
 		try {
